47e47a0303dbe0a34b236ece1e8bb06ff2f74dbc
JustinTadlock <justin.b.tadlock@gmail.com>
2019-08-22 20:22:32 -0700

    Adding addItem layout
    Adding paths and dummy data for testing add item layout

--- start_catalog_server.py
+++ start_catalog_server.py
@@ -10,14 +10,54 @@ from flask import (
 app = Flask(__name__)
 app.secret_key = "Replace me"
 
+# Temp Data
+main_categories = ['Outdoors', 'Video Games', 'Technology', 'Kitchen']
+sub_categories = {
+    "Outdoors": [
+        'Boots',
+        'Tents',
+        'Backpacks',
+        'Gloves',
+        'Coolers'
+    ],
+    "Video Games": [
+        'Consoles',
+        'Games',
+        'Accessories'
+    ],
+    "Technology": [
+        'Desktops',
+        'Laptops',
+        'Monitors',
+        'Keyboards',
+        'Mice'
+    ],
+    "Kitchen": [
+        'Pots and Pans',
+        'Cups',
+        'Silverware',
+        'Coffee Makers'
+    ]
+}
 
 @app.route("/")
 def Index():
     return render_template('index.html', title="Catalog")
 
+
 @app.route('/layout/index')
-def Layout():
+def Index_Layout():
     return render_template('index_layout.html')
+
+
+@app.route('/layout/addItem')
+def AddItem_Layout():
+    return render_template(
+        'addItem_layout.html', 
+        main_categories=main_categories, 
+        sub_categories=sub_categories
+    )
+
 
 
 if __name__ == "__main__":
