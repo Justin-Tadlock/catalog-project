eca3740800894a048979c55d448cf4fe80f2fa90
JustinTadlock <justin.b.tadlock@gmail.com>
2019-08-23 21:55:19 -0700

    Add the database setup logic
    Creating User, Category, Sub_Category, and Item tables for the item catalog db.

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,88 @@
-# Add database definitions here
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+class Category(Base):
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class Sub_Category(Base):
+    __tablename__ = "sub_category"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    cat_id = Column(Integer, ForeignKey('category.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'cat_id': self.cat_id
+        }
+
+class Item(Base):
+    __tablename__ = "item"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    price = Column(String(10), nullable=False)
+    category = Column(String(50), nullable=False)
+    sub_category = Column(String(50), nullable=False)
+    picture = Column(String(1000), nullable=True)
+    link = Column(String(1000), nullable=True)
+    description = Column(String(255), nullable=False)
+
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    sub_cat_id = Column(Integer, ForeignKey('sub_category.id'))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'category': self.category,
+            'sub_category': self.sub_category,
+            'picture': self.picture,
+            'link': self.link,
+            'description': self.description,
+            'cat_id': self.cat_id,
+            'sub_cat_id': self.sub_cat_id
+        }
+
+### Insert at end of file ###
+engine = create_engine('sqlite:///item_catalog.db')
+
+Base.metadata.create_all(engine)
